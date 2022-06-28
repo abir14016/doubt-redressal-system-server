998f5d360d927e5422edf81929b064393e689b59
abir14016 <abir14016@gmail.com>
2022-06-28 23:28:31 +0600

    teacher and admin api

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Accesss' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         await client.connect();
@@ -98,6 +113,44 @@ async function run() {
             });
             res.send({ result, token });
         });
+
+
+        //load all users
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            return res.send(users);
+        });
+
+
+        //onlyteacher api
+        app.get('/teacher/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isTeacher = user.role === 'teacher';
+            res.send({ teacher: isTeacher })
+        });
+
+
+        // onlyUser api
+        app.get('/student/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isStudent = user.role === 'student';
+            res.send({ student: isStudent })
+        });
+
+
+
+        //load user based on email
+        // app.get('/user/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email: email };
+        //     const cursor = userCollection.findOne(query);
+        //     const user = await cursor.toArray();
+        //     return res.send(user);
+        // });
 
     }
     finally {
